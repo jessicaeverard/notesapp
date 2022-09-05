f554a4a0ecb2f7fb8f18d95f43ec158f1cb992bd
Jess <jessica@mediatrader.com>
2022-09-05 18:29:17 +0100

    first template

--- notesapp/urls.py
+++ notesapp/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from notes import views as notesViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', notesViews.index),
 ]

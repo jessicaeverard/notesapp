5022fefd862737240eb8c5ce981393798c27a744
Jess <jessica@mediatrader.com>
2022-09-12 17:56:35 +0100

    code now working

--- notes/models.py
+++ notes/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Notes(models.Model):    
     text = models.CharField(max_length=500)    
     date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
--- notes/views.py
+++ notes/views.py
@@ -5,14 +5,19 @@ from .models import Notes
 
 
 def index(request):
-    notes = Notes.objects.all()
+    if request.method == 'POST': #method is post as we are inputting data
+        form = NoteForm(request.POST) #creating an instance for this form
+        newNote = form.save(commit=False) #creating the object but not committing yet - may not be needed
+        #newNote.text = request.text, not too sure why this is not needed maybe cuz its a post request?
+        newNote.save() #creating the note object
+        notes = Notes.objects.all()
+        return render(request, "home.html", {'form':NoteForm, 'notes':notes})
 
+    else:
+        notes = Notes.objects.all()
     return render(request, "home.html", {'form':NoteForm, 'notes':notes})
 
 
-def createnote(request):
-    form = NoteForm(request.POST)
-    newNote = form.save(commit=False)
-    newNote.text = request.text
-    newNote.save()
-    return redirect('home.html', newNote)
+
+
+
